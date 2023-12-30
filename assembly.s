e2d0336c07c9b20cfb75dd8ecf5666b8fb0bee89
Kyle Lee <kylelee.sea@gmail.com>
2023-12-30 12:40:47 -0800

    Phase 3 complete except for division code gen

--- assembly.s
+++ assembly.s
@@ -1,4 +1,12 @@
 .globl _main
 _main:
-movl $0, %eax
+movl $1, %eax
+push %rax
+movl $2, %eax
+pop %rcx
+subl %rax, %rcx
+push %rax
+movl $3, %eax
+pop %rcx
+subl %rax, %rcx
 ret
--- src/gen.rs
+++ src/gen.rs
@@ -2,6 +2,7 @@ use std::fs::OpenOptions;
 use std::fs::File;
 use std::io::Write;
 use super::types::ASTTree;
+use super::types::Token;
 use std::fs;
 use std::path::Path;
 use std::process::Command;
@@ -51,14 +52,41 @@ fn process_statement(tree: ASTTree, file: File) {
     }
 }
 
-fn process_return(tree: ASTTree, mut file: File) {
+fn process_expression(tree: ASTTree, mut file: &File) {
     match tree {
         ASTTree::Constant(x) => {
             write_wrapper(write!(file, "movl ${}, %eax\n", x));
-            write_wrapper(write!(file, "{}", "ret\n"));
         },
+        ASTTree::UnaryOp(op, child) => {
+            write_wrapper(write!(file, "{}\n", "unary op code, andrew do things
+            here"));
+        },
+        ASTTree::BinaryOp(left, op, right) => {
+            process_expression(*left, &file);
+            write_wrapper(write!(file, "push %rax\n"));
+            process_expression(*right, &file);
+            // e1 in rcx, e2 in rax
+            write_wrapper(write!(file, "pop %rcx\n"));
+            match op {
+                Token::TAdd => write_wrapper(write!(file, "addl %rcx, %rax\n")),
+                Token::TMultiply =>
+                    write_wrapper(write!(file, "imul %rcx, %rax\n")),
+                // subl src, dst... eax is e2 rcx is e1
+                Token::TNeg => write_wrapper(write!(file, "subl %rax, %rcx\n")),
+                Token::TDivide => {
+                    write_wrapper(write!(file, "division not yet impl\n"));
+                },
+                _ => panic!("Invalid operator found in binaryOp code gen"),
+            }
+
+        }
         _ => invalid_match("process return"),
     }
+}
+
+fn process_return(tree: ASTTree, mut file: File) {
+    process_expression(tree, &file);
+    write_wrapper(write!(file, "{}", "ret\n"));
 }
 
 fn write_wrapper(res: Result<(), std::io::Error>) {
